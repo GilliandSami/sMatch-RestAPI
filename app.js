8cbc316269b061c501382dc46b50b672846ac73d
morgui2003 <devalsmorgane@gmail.com>
2024-11-13 14:01:32 +0100

    3.0.3 - ajout et implementation  du document openapi et modifications dans les fichiers app et package. correction du fichier imageUpload

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const swaggerUi = require('swagger-ui-express');
 const dotenv = require('dotenv');
 const connectDB = require('./bin/db');
 const routes = require('./routes');
@@ -9,6 +12,11 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use('/api', routes);
+
+// Analyse le document OpenAPI.
+const openApiDocument = yaml.load(fs.readFileSync('./openapi.yml'));
+// Sert la documentation Swagger UI.
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
 // Pour du test :
 // app.get('/', (req, res) => {
--- middleware/imageUpload.js
+++ middleware/imageUpload.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../config/cloudinaryConfig');
+const cloudinary = require('../bin/cloudinaryConfig');
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
