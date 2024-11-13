7e23aeeb0e7ea3288583d387d46b719e875a47fc
Sami Gilliand <sami.gilliand@heig-vd.ch>
2024-11-13 14:17:00 +0100

    3.0.1 - Corrections des erreurs YAML pour la documentation - Ajout d'une route racine dans app.js à l'accès de la documentation

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const fs = require('fs');
-const yaml = require('js-yaml');
-const swaggerUi = require('swagger-ui-express');
 const dotenv = require('dotenv');
 const connectDB = require('./bin/db');
 const routes = require('./routes');
+
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 
 dotenv.config();
 connectDB();
@@ -13,10 +14,8 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-// Analyse le document OpenAPI.
-const openApiDocument = yaml.load(fs.readFileSync('./openapi.yml'));
-// Sert la documentation Swagger UI.
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
+const openApiDocument = YAML.load('./swagger.yaml');
+app.use('/', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
 // Pour du test :
 // app.get('/', (req, res) => {
