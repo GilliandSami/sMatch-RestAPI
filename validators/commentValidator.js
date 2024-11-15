119dd16a24e458856537877b4974bc3fd59c6861
Sami Gilliand <sami.gilliand@heig-vd.ch>
2024-11-15 13:40:37 +0100

    3.0.3 - Finalisation de la documentation de l'API Rest sMatch - Ajout de la documentation de toutes les routes, des paramètres, les en-têtes, les corps de requête et les réponses possibles

--- validators/commentValidator.js
+++ validators/commentValidator.js
@@ -5,9 +5,11 @@ exports.validateCommentCreation = [
     body('post').notEmpty().withMessage('Le post associé est requis'),
     (req, res, next) => {
         const errors = validationResult(req);
+
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
         next();
     }
 ];
